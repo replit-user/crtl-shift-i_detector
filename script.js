3258e465f8eb36689f935978da68c438d8e5d09f
replit-user <umyashinderu@gmail.com>
2024-12-17 10:29:40 -0700

    Create script.js
    created da file

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown",function(event){
+                if(event.ctrlKey&&event.shiftKey&&event.key.toLowerCase()==="i"){
+                    //your code here
+                    event.preventDefault()
+                }
+            })
